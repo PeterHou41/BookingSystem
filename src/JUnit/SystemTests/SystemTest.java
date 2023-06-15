49861a680746d061a183fb591d2d511e0fc805ac
th214 <th214@pc7-018-l.cs.st-andrews.ac.uk>
2023-06-15 13:20:47 +0100

    Added more basic tests

--- src/JUnit/SystemTests/SystemTest.java
+++ src/JUnit/SystemTests/SystemTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import src.model.*;
 
+import java.util.List;
+
 public class SystemTest {
 
 
@@ -16,35 +18,113 @@ public class SystemTest {
     @Before
     public void SystemTest() {
         bookingSystem = new BookingSystem();
+        bookingSystem.addBuilding(
+                "John Honey Building", "University of, John Honey Building, N Haugh, St Andrews KY16 9SX");
     }
 
     @Test
-    public void testWrongPersonInfo() {
-        boolean valid0 = bookingSystem.checkPersonInfoValidity("Joseph Biden", "bjohnson77@gov.uk");
+    public void testInvalidPersonInfo() {
+        boolean valid0 = bookingSystem.checkPersonInfoValidity("Joseph Biden", "");
         assertFalse(valid0);
         boolean valid1 = bookingSystem.checkPersonInfoValidity("", "whatever@testEmptyName");
         assertFalse(valid1);
     }
 
     @Test
-    public void testCorrectPersonInfo() {
-        boolean valid2 = bookingSystem.checkPersonInfoValidity("Donald Trump", "dtrump45@usa.gov");
-        assertTrue(valid2);
+    public void testValidPersonInfo() {
+        boolean valid0 = bookingSystem.checkPersonInfoValidity("Boris Johnson", "bjohnson77@gov.uk");
+        assertTrue(valid0);
+        boolean valid1 = bookingSystem.checkPersonInfoValidity("Donald Trump", "dtrump45@usa.gov");
+        assertTrue(valid1);
     }
 
     @Test
-    public void testEmptyBuildingInfo() {
-        boolean valid3 = bookingSystem.checkBuildingInfoValidity("", "North Haugh");
-        assertFalse(valid3);
-        boolean valid4 = bookingSystem.checkBuildingInfoValidity("Jack Cole Building", "");
-        assertFalse(valid4);
+    public void testInvalidBuildingInfo() {
+        boolean invalid0 = bookingSystem.checkBuildingInfoValidity("", "North Haugh");
+        assertFalse(invalid0);
+        boolean invalid1 = bookingSystem.checkBuildingInfoValidity("Jack Cole Building", "");
+        assertFalse(invalid1);
     }
 
     @Test
-    public void testCorrectBuildingInfo() {
-        boolean valid5 = bookingSystem.checkBuildingInfoValidity("Jack Cole Building", "North Haugh");
-        assertTrue(valid5);
+    public void testValidBuildingInfo() {
+        boolean valid0 = bookingSystem.checkBuildingInfoValidity(
+                "Jack Cole Building", "Jack Cole Building, N Haugh, St Andrews KY16 9SX");
+        assertTrue(valid0);
+        boolean valid1 = bookingSystem.checkBuildingInfoValidity("PM's Office", "10 Downing Street");
+        assertTrue(valid1);
     }
+
+    // John Honey Building was already added to the system.
+    @Test
+    public void testValidRoomInfo() {
+
+        boolean valid0 = bookingSystem.checkRoomInfoValidity("JH109 Quiet Lab", 1);
+        assertTrue(valid0);
+        boolean valid1 = bookingSystem.checkRoomInfoValidity("JH110 Teaching Lab", 1);
+        assertTrue(valid1);
+    }
+
+    @Test
+    public void testInvalidRoomInfo() {
+
+        boolean invalid0 = bookingSystem.checkRoomInfoValidity("", 1);
+        assertFalse(invalid0);
+
+        bookingSystem.addRoom("JH109 Quiet Lab", 1);
+        boolean invalid1 = bookingSystem.checkRoomInfoValidity("JH109 Quiet Lab", 1);
+        assertFalse(invalid1);
+    }
+
+    @Test
+    public void testPeopleNum() {
+        bookingSystem.addPerson("Boris Johnson", "bjohnson77@gov.uk");
+        bookingSystem.addPerson("Donald Trump", "dtrump45@usa.gov");
+
+        List<Person> people = bookingSystem.getPeople();
+        boolean valid0 = (people.size() == 2);
+        assertTrue(valid0);
+
+        bookingSystem.removePerson(2);
+        bookingSystem.addPerson("Rowan Atkinson", "mrbean91@gmail.com");
+        boolean valid1 = (people.size() == 2);
+        assertTrue(valid1);
+    }
+
+    // John Honey Building was already added to the system.
+    @Test
+    public void testBuildingNum() {
+        bookingSystem.addBuilding("Younger Hall", "St Andrews North Street KY16 9AJ");
+        bookingSystem.addBuilding(
+                "University Sports Centre",
+                "Saints Sport, University Park, St Leonard's Rd, St Andrews KY16 9DY");
+
+        List<Building> buildings = bookingSystem.getBuildings();
+        boolean valid0 = (buildings.size() == 3);
+        assertTrue(valid0);
+
+        bookingSystem.removeBuilding(2);
+        boolean valid1 = (buildings.size() == 2);
+        assertTrue(valid1);
+    }
+
+    @Test
+    public void testRoomNum() {
+
+        bookingSystem.addRoom("JH109 Quiet Lab", 1);
+        bookingSystem.addRoom("JH110 Teaching Lab", 1);
+        bookingSystem.addRoom("JH108 PhD Candidates Offices", 1);
+
+        List<Room> rooms = bookingSystem.getRooms();
+        boolean valid0 = (rooms.size() == 3);
+        assertTrue(valid0);
+
+        bookingSystem.removeRoom(2);
+        boolean valid1 = (rooms.size() == 2);
+        assertTrue(valid1);
+    }
+
+
 
 
 }
